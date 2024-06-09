6f811e4d4123f879caaba8a1cb0444a89f119aa3
kimbongju02 <qhdwn25@gmail.com>
2024-06-09 17:49:08 +0900

    pyqt_rsa

--- pyqt_rsa_2.py
+++ pyqt_rsa_2.py
@@ -5,8 +5,13 @@ from base64 import b64encode, b64decode
 class RSACryptoApp(QWidget):
     def __init__(self):
         super().__init__()
+        self.p = 13
+        self.q = 29
+        self.n = self.p * self.q
+        self.tot = (self.p - 1) * (self.q - 1)
+        self.e = self.generate_public_key()
+        self.d = self.generate_private_key()
         self.initUI()
-        self.generate_keys()
 
     def initUI(self):
         self.layout = QVBoxLayout()
@@ -44,84 +49,44 @@ class RSACryptoApp(QWidget):
         self.setGeometry(300, 300, 400, 300)
         self.show()
 
+    def gcd(self, a, b):
+        while b != 0:
+            a, b = b, a % b
+        return a
+
+    def generate_public_key(self):
+        e = 2
+        while e < self.tot and self.gcd(e, self.tot) != 1:
+            e += 1
+        return e
+
+    def generate_private_key(self):
+        d = 1
+        while (self.e * d) % self.tot != 1 or d == self.e:
+            d += 1
+        return d
+
     def encrypt_text(self):
         plain_text = self.input_text.text()
-        encrypted_text = encrption(plain_text)
-        encrypted_text = b64encode(encrypted_text).decode('utf-8')
+        encrypted_text = self.encrypt(plain_text)
+        encrypted_text = b64encode(encrypted_text.encode('utf-8')).decode('utf-8')
         self.encrypted_text.setPlainText(encrypted_text)
 
     def decrypt_text(self):
         encrypted_text = self.encrypted_text.toPlainText()
         encrypted_bytes = b64decode(encrypted_text)
-        decrypted_text=decryption(encrypted_bytes)
-        decrypted_text = decrypted_text.decode('utf-8')
+        decrypted_text = self.decrypt(encrypted_bytes.decode('utf-8'))
         self.decrypted_text.setPlainText(decrypted_text)
+
+    def encrypt(self, plain_text):
+        cipher_text = [chr((ord(char) ** self.e) % self.n) for char in plain_text]
+        return ''.join(cipher_text)
+
+    def decrypt(self, cipher_text):
+        plain_text = [chr((ord(char) ** self.d) % self.n) for char in cipher_text]
+        return ''.join(plain_text)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = RSACryptoApp()
     sys.exit(app.exec_())
-
-
-p = 13
-q = 29
-n = p * q
-tot = (p - 1) * (q - 1)
-#최대공약수 구하는 거
-def gcd(num1, num2):
-    while num2 != 0:
-        num1, num2 = num2, num1%num2
-    return num1
-
-#공개키
-def publickey():
-    global tot
-    e = 2
-    while e<tot and gcd(e, tot) != 1:
-        e += 1
-    return e
-
-#개인키
-def privatekey():
-    global e
-    global tot
-    d = 1
-    while (publickey() * d) % tot != 1 or d == publickey():
-        d += 1
-    return d 
-    
-def encrption(ori):
-    oris = list(ori)
-    orior = []
-    eori = []
-    etext = ""
-    
-    for orisc in range(0, len(oris)):
-        orior.append(ord(oris[orisc]))
-    for oriorc in range(0, len(orior)):
-        eori.append(((orior[oriorc]**publickey())%n))
-    
-    for eoric in range(0, len(eori)):
-        etext += (chr(eori[eoric]))
-    
-    return etext
-
-def decryption(eori):
-    d = privatekey()
-    
-    eoris = list(eori)
-    eroiso = []
-    eroisor = []
-    eroar = []
-    text = ""
-    
-    for eorisc in range(0, len(eoris)):
-        eroiso.append(ord(eoris[eorisc]))
-    for eroisoc in range(0, len(eroiso)):
-        eroisor.append((eroiso[eroisoc]**d)%n)
-    for eroisorc in range(0, len(eroisor)):
-        eroar.append(chr(eroisor[eroisorc]))
-    for eroarc in range(0, len(eroar)):
-        text += eroar[eroarc]
-
-    return text
